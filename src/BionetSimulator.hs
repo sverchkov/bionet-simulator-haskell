c4833b99b7d4b73e9bc1e871346e2ad5b4aeb332
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2014-10-24 18:13:45 -0500

    Redesign network data structure (polarity, no interaction grouping) Separated string parsing out.

--- src/BionetSimulator.hs
+++ src/BionetSimulator.hs
@@ -12,80 +12,56 @@ type Node = String
 
 type EdgeType = String
 
-data Edge = Edge { source :: Node
-                 , dest :: Node
-                 , edgeType :: EdgeType
-                 } deriving (Show)
+-- Some typeclass decisions for (edge) polarity:
+-- not Ord (b/c I don't think it makes sense to compare polarities w/ gt/lt)
+-- not Bounded (same reasoning, no sense in thinking one is smaller than the other)
+-- not Enum (Don't think it makes sense to think of one polarity as having the other as a predecessor)
+data Polarity = Activating | Inhibiting deriving (Eq, Show, Read)
 
-type InteractionType = String
+-- Probably makes sense to couple polarity in the interaction type
+type InteractionType = (EdgeType, Polarity)
 
--- Map of node to interaction type + parent set.
-type Net = Map.Map Node (Either (InteractionType, [Node]) [(InteractionType, Node)])
+-- A net is a map of node to a list of interaction type - parent pairs
+type Net = Map.Map Node [(InteractionType, Node)]
 
 -- Map of node to expression level
 type Value = Double
 type Values = Map.Map Node Value
-type InteractionSpec = InteractionType -> [Value] -> Value
-
--- Constructors from strings
---------------------------------------------------------------------------------------------------
-
--- Take in cytoscape-like description of network, and get out list-of-edges description of network
-mkLOE :: String -> [Edge]
-mkLOE = (map mkEdge) . lines
-
-mkEdge :: String -> Edge
-mkEdge inStr = Edge { source = src, dest = dst, edgeType = et } where
-    (src : et : dst :_) = words inStr
-
--- Turn list of edges into a "network" representation (Map based).
-mkNet :: [Edge] -> Net
-mkNet [] = Map.empty
-mkNet (edge:rest) = Map.unionWith parentMerge (netFromEdge edge) (mkNet rest) where
-
-    parentMerge (Left ("", [])) x = x
-    parentMerge (Left (it1, parents1)) (Left (it2, parents2)) =
-        if it1 == it2 || it2 == "" then
-            -- I'm just concatenating parent lists. Don't know if I should union instread.
-            Left (it1, parents1++parents2) else
-            parentMerge (rightify (it1, parents1)) (rightify (it2, parents2))
-    parentMerge (Left x) (Right l) = parentMerge (rightify x) (Right l)
-    parentMerge y (Left x) = parentMerge (Left x) y
-    parentMerge (Right l1) (Right l2) = Right $ l1++l2
-
-    rightify (it, parents) = Right $ zip (repeat it) parents
-
-    netFromEdge edge = Map.insert (dest edge) (Left ((edgeType edge),[source edge])) tmpMap where
-        tmpMap = Map.singleton (source edge) (Left ([],[]))
-
--- Turn integaction spec file into interaction spec
-readInteractionSpec :: String -> InteractionSpec
-readInteractionSpec _ = f where f _ = sum -- Stand-in. Need to fill in deets.
+type InteractionSpec = InteractionType -> Value -> Value
+type LinkFn = [Value] -> Value
 
 -- Network inspection
 ---------------------
 
--- Utility to partition interactions into mixed and unmixed
-mixedInteractions :: Net -> ([(Node, Either (InteractionType, [Node]) [(InteractionType, Node)])], [(Node, Either (InteractionType, [Node]) [(InteractionType, Node)])])
-mixedInteractions net = List.partition hasRight $ Map.assocs net where
-    hasRight (_, Right _) = True
-    hasRight _ = False
-
-prettyPrinter :: [(Node, Either (InteractionType, [Node]) [(InteractionType, Node)])] -> String
-prettyPrinter theList = unlines $ map purdyPrint theList where
-    purdyPrint (node, Right list) = (unlines (map show list)) ++ "->" ++ (show node)
-    purdyPrint (node, Left (it, list)) = (unlines (map show list)) ++ "->" ++ (show it) ++ " ->" ++ (show node)
-
 -- List all interaction types
-interactionTypes :: Net -> [InteractionType]
-interactionTypes net = foldl List.union [] $ map getIT $ Map.assocs net where
-    getIT (node, Right list) = map fst list
-    getIT (node, Left (it, _)) = [it]
+edgeTypes :: Net -> [EdgeType]
+edgeTypes net = foldl List.union [] $ map cleanup $ Map.assocs net where
+    cleanup (_, list) = map (fst.fst) list
+
+-- General computation framework
+--------------------------------
 
 -- Running a computation really consists of querying a values map from a network and an interaction spec.
-mkValues :: Net -> InteractionSpec -> Values
-mkValues net intspec = values where
-    values = Map.map evaluator net
-    evaluator (Left (it, parents)) = intspec it (map (values Map.!) parents)
-    evaluator (Right list) = 0 --TODO: Make meaningful!
+computeValues :: Net -> Map.Map Node Value -> LinkFn -> InteractionSpec -> Values
+computeValues net fixed link intspec = values where
 
+    values = Map.mapWithKey evaluatorOuter net
+
+    evaluatorOuter node list = if Map.member node fixed
+        then fixed Map.! node
+        else link $ map (evaluator node) list
+
+    evaluator :: Node -> (InteractionType, Node) -> Value
+    evaluator node (interaction, parent) = intspec interaction (values Map.! parent) -- Ignores the node, for now :/
+
+-- The dummiest defaults
+------------------------
+
+-- Link function = 2 expit - 1
+scaledExpit :: LinkFn 
+scaledExpit list = 2/(1+exp(-sum list)) - 1
+
+-- Interaction spec = polarity (as +/- 1) times 4
+polarityInteractionSpec :: InteractionSpec
+polarityInteractionSpec (_, Activating) = (4*)
+polarityInteractionSpec (_, Inhibiting) = ((-4)*)
